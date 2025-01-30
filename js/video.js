f1cbf91b0246f54415deaee474fdf3a110bc99cb
Manuel Perez. <92071461+manuelp0635@users.noreply.github.com>
2025-01-30 09:59:36 -0500

    Delete js directory

--- js/video.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    if ($(window).width() > 1025) {
-      $('#large-video').attr('autoplay', 'autoplay');
-    } else {
-      $('#small-video').attr('autoplay', 'autoplay');
-    }
-  });
